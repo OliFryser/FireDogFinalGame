0f7ee33b3f5a5a4bd197a30840ebe5bfaad467e7
Lignio <olfw@itu.dk>
2024-10-31 14:44:51 +0100

    updated Combat Protoype with couch animations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,10 +9,16 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private float _movementSpeed = 50.0f;
     private Vector2 _direction;
+
+    public Vector2 PreviousDirection;
     private Rigidbody2D _rigidBody2D;
     private Animator _animator;
 
     public bool FacingRight;
+
+    public bool FacingUp;
+
+    private bool _isMoving => _direction.sqrMagnitude > 0.01f;
 
     void Start()
     {
@@ -22,7 +28,15 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
+
+        if (_isMoving) 
+            PreviousDirection = _direction;
+
+        
         _rigidBody2D.AddForce(_direction * _movementSpeed);
+
+        IsFacingUp();
+        Debug.Log("Facing up?: " + FacingUp.ToString());
         FlipSprite();
         UpdateAnimator();
     }
@@ -30,7 +44,7 @@ public class Movement : MonoBehaviour
     private void UpdateAnimator()
     {
         _animator.SetFloat("Speed", _direction.sqrMagnitude);
-        if (_direction.sqrMagnitude > 0.01f)
+        if (_isMoving)
         {
             _animator.SetFloat("Horizontal", _direction.x);
             _animator.SetFloat("Vertical", _direction.y);
@@ -41,6 +55,8 @@ public class Movement : MonoBehaviour
     {
         if (Math.Abs(_direction.x) < 0.01f) return;
         FacingRight = _direction.x > 0;
+        //Debug.Log ("Direction x: " + _direction.x.ToString());
+        //Debug.Log("Direction y: " + _direction.y.ToString());
         _spriteRenderer.flipX = FacingRight;
     }
 
@@ -48,5 +64,17 @@ public class Movement : MonoBehaviour
     void OnMove(InputValue input)
     {
         _direction = input.Get<Vector2>();
+        //Debug.Log ("Direction: " + _direction.ToString());
+        //Debug.Log ("Direction x: " + _direction.x.ToString());
+        //Debug.Log("Direction y: " + _direction.y.ToString());
+    }
+
+    void IsFacingUp() {
+        if (_direction.y > 0) {
+            FacingUp = true;
+        } 
+        else if (_direction.y < 0 ) {
+            FacingUp = false;
+        }
     }
 }
