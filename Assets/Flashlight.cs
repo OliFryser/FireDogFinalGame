8678e86a01a58769f377d904dc55ecdcd01ac720
OliFryser <oliver-1999@live.dk>
2024-11-07 12:09:02 +0100

    add flashlight and spooky ambient lighting

---
+++ Assets/Flashlight.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Flashlight : MonoBehaviour
+{
+    private Light2D _light;
+
+    private void Start()
+    {
+        _light = GetComponent<Light2D>();
+    }
+
+    public void TurnOffFlashlight()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void TurnOnFlashlight()
+    {
+        gameObject.SetActive(true);
+    }
+}
--- Assets/Scripts/SceneManagement/RoomManager.cs
+++ Assets/Scripts/SceneManagement/RoomManager.cs
@@ -5,16 +5,29 @@ public class RoomManager : MonoBehaviour
     [SerializeField]
     private UpgradeMenu _upgradeMenu;
 
+    [SerializeField]
+    private GameObject _warmLight;
+
+    [SerializeField]
+    private GameObject _spookyLight;
+
     private InputLock _inputLock;
+
+    private Flashlight _flashlight;
+
     private void Start()
     {
         _inputLock = FindAnyObjectByType<InputLock>();
+        _flashlight = FindAnyObjectByType<Flashlight>();
     }
 
     public void ClearRoom()
     {
         _upgradeMenu.Show();
         _inputLock.LockInput();
+        _spookyLight.SetActive(false);
+        _warmLight.SetActive(true);
+        _flashlight.TurnOffFlashlight();
     }
 
     public void CloseUpgradeMenu()
