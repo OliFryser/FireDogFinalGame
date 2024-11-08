cd86ff401c03cd95e9082a858285889b3007e260
OliFryser <oliver-1999@live.dk>
2024-11-08 14:13:23 +0100

    change logic to load by scene index

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -3,18 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] private UnityEditor.SceneAsset level1Scene; // SceneAsset for Inspector
-
     public void StartGame()
     {
-        if (level1Scene != null)
-        {
-            string sceneName = level1Scene.name;
-            SceneManager.LoadScene(sceneName);
-        }
-        else
-        {
-            Debug.LogError("Level 1 scene is not assigned.");
-        }
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
