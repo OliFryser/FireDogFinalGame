76633c9dd189a9c6af035a83eb3dda25e1d924bd
OliFryser <oliver-1999@live.dk>
2024-11-13 21:21:58 +0100

    fix bug, that causes the enemy tracker to count wrongly

--- Assets/Scripts/Enemy/EnemyHitDetection.cs
+++ Assets/Scripts/Enemy/EnemyHitDetection.cs
@@ -56,8 +56,12 @@ public class EnemyHitDetection : MonoBehaviour
 
         if (_health <= 0)
         {
-            _enemyTracker.UnregisterEnemy();
             Destroy(gameObject);
         }
+    }
+
+    private void OnDestroy()
+    {
+        _enemyTracker.UnregisterEnemy();
     }
 }
